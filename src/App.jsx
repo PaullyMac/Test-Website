04e208f80be36612f3d5ee9caf69530bcb186745
Paul Macaraeg <116135517+PaullyMac@users.noreply.github.com>
2023-09-21 11:45:02 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Paul Macaraeg",
+  title: "Aspiring Data Analyst",
+  email: "paulmacaraeg24@gmail.com",
+  gitHub: "PaullyMac",
+  instagram: "magnanimouspaully",
+  linkedIn: "paul-macaraeg",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "pestilentpaully",
+  youTube: "The Paul TV",
 };
 
 const primaryColor = "#4E567E";
